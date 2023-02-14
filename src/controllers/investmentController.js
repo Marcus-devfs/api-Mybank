1174f3a7fd657d418b83884d4a3440c49d450bbb
Marcus-devfs <marcusvini6277@gmail.com>
2023-02-14 11:18:12 -0300

    att

--- src/controllers/investmentController.js
+++ src/controllers/investmentController.js
@@ -65,6 +65,53 @@ class InvestmentListController {
         }
     }
 
+    listFilterInvestiments = async (req, res) => {
+        const { date_start = null, date_finished = null, user_find = null } = req.body
+
+        let filter = {
+            date_inicio: date_start != '' ? date_start : null,
+            date_fim: date_finished != '' ? date_finished : null,
+            id_user: user_find
+        }
+
+        typeof date_start == 'undefined' || date_start == '' || date_start == null && delete filter.date_start
+        typeof date_finished == 'undefined' || date_finished == '' || date_finished == null && delete filter.date_finished
+
+        //filtro data
+        if (filter.date_inicio != undefined) {
+            filter.date = {}
+            let data_in = date_start.split('/')
+            var date_start_filter = new Date(new Date(data_in[2], parseInt(data_in[1]) - 1, parseInt(data_in[0]), 0, 0, 0, 0));
+
+            if (filter.date_fim != null) {
+                let data_fim = date_finished.split('/')
+                var d_fim = new Date(new Date(data_fim[2], parseInt(data_fim[1]) - 1, parseInt(data_fim[0]), 0, 0, 0, 0));
+
+                filter.date = {
+                    $gte: date_start_filter,
+                    $lte: d_fim
+                }
+            }
+
+        } else {
+            if (filter.id_user != null) {
+                filter.user = filter.id_user
+            }
+        }
+
+        if (filter.id_user != null) {
+            filter.user = filter.id_user
+        }
+
+        await InvestmentList.find(filter)
+            .then(async doc => {
+                res.status(200).json(doc);
+            })
+            .catch(err => {
+                res.status(500).json({ error: err });
+            });
+    }
+
 }
 
 module.exports = new InvestmentListController()
